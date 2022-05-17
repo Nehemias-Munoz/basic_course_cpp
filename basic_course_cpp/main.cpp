a3dc6f11d1cd19fa8e5c13dc8aaa5d204c5848c1
Nehemias-Munoz <nehemiasmunozm@gmail.com>
2022-05-17 15:46:09 -0400

    Uso de apuntadores

--- basic_course_cpp/main.cpp
+++ basic_course_cpp/main.cpp
@@ -9,13 +9,20 @@
 using namespace::std;
 
 int main(int argc, const char * argv[]) {
-    // uso de variables
-    
-    int userAge = 22;
-    string userCountry;
-    userCountry = "Chile";
-    string userName = "Nehemias Munoz";
-    cout << " Mi nombre es " << userName << " y Tengo "<< userAge << endl;
-    
-    
+    // Uso de apuntadores
+    /*
+     el uso de apuntadores es comun al querer cambiar valores de variables dentro de funciones externas a la cual
+     se declaro la variable
+     */
+    // creacion y asignacion de variable
+    int userHomeAdress = 2084;
+    cout << "Direccion usuario: "<< userHomeAdress << endl;
+    // creacion de variable apuntador ( de variable userHomeAdress): que muestra direccion en memoria
+    int* prompterUserAdress = &userHomeAdress;
+    cout << "Direccion en memoria de variable anterior: " << prompterUserAdress<< endl;
+    // cambiando valor de variable userHomeAdress directamente con el numero de la varible en memoria
+    *prompterUserAdress = 1000;
+    cout << "Direccion nueva usuario: "<< userHomeAdress << endl;
+
+
 }
