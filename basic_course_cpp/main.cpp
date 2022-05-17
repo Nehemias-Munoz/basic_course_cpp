011328c924198eaa6cf3cb8c4d39efb5a8330d7d
Nehemias-Munoz <nehemiasmunozm@gmail.com>
2022-05-17 15:19:20 -0400

    Uso e impresión de variables

--- basic_course_cpp/main.cpp
+++ basic_course_cpp/main.cpp
@@ -9,19 +9,13 @@
 using namespace::std;
 
 int main(int argc, const char * argv[]) {
-    // tipos de datos primitivos
-    //int
-    cout << 10 << endl;
-    //float
-    cout << 3.1416f << endl;
-    //bool
-    cout << true << endl;
-    //char
-    cout << "H" << endl;
+    // uso de variables
     
-    //Datos compuestos
+    int userAge = 22;
+    string userCountry;
+    userCountry = "Chile";
+    string userName = "Nehemias Munoz";
+    cout << " Mi nombre es " << userName << " y Tengo "<< userAge << endl;
     
-    //string
-    cout << "Hola \n" << endl; // \n = termino de linea
-
+    
 }
