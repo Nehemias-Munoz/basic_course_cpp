5411a6b383d5b704f813d49ff44eb3f9c6bf3511
Nehemias-Munoz <nehemiasmunozm@gmail.com>
2022-05-17 16:08:57 -0400

    Primer ejercicio

---
+++ basic_course_cpp/Exercises.hpp
@@ -0,0 +1,13 @@
+//
+//  Exercises.hpp
+//  basic_course_cpp
+//
+//  Created by Nehemias Muñoz on 17-05-22.
+//
+
+#ifndef Exercises_hpp
+#define Exercises_hpp
+
+#include <stdio.h>
+
+#endif /* Exercises_hpp */
--- basic_course_cpp/main.cpp
+++ basic_course_cpp/main.cpp
@@ -9,20 +9,31 @@
 using namespace::std;
 
 int main(int argc, const char * argv[]) {
-    // Uso de apuntadores
-    /*
-     el uso de apuntadores es comun al querer cambiar valores de variables dentro de funciones externas a la cual
-     se declaro la variable
-     */
-    // creacion y asignacion de variable
-    int userHomeAdress = 2084;
-    cout << "Direccion usuario: "<< userHomeAdress << endl;
-    // creacion de variable apuntador ( de variable userHomeAdress): que muestra direccion en memoria
-    int* prompterUserAdress = &userHomeAdress;
-    cout << "Direccion en memoria de variable anterior: " << prompterUserAdress<< endl;
-    // cambiando valor de variable userHomeAdress directamente con el numero de la varible en memoria
-    *prompterUserAdress = 1000;
-    cout << "Direccion nueva usuario: "<< userHomeAdress << endl;
+//   Ejercicio #1
+    
+// Calculadora de Areas
+    short int userSelecction;
+    float userRadio;
+    short int area;
+    const float pi = 3.1416f;
+    cout << "Bievenido a la calculadora de areas \n Seleccione una opcion para comenzar: \n 1 - Area de un circulo \n 2 - Area de una esfera \n";
+    cin >> userSelecction;
+    switch (userSelecction) {
+        case 1:
+            cout << "Ingrese el radio de su circulo: ";
+            cin >> userRadio;
+            area = (userRadio*2) * pi;
+            cout << "El area de tu circulo es: "<< area << endl;
+            break;
+        case 2:
+            cout << "Ingrese el radio de su esfera: ";
+            cin >> userRadio;
+            area = (userRadio*2) * (pi * 4);
+            cout << "El area de tu esfera es: "<< area << endl;
+        default:
+            cout << "Ingrese un valor dentro del rango de opciones.";
+            break;
+    }
 
 
 }
