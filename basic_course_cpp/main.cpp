468b5c3712d32de400971f76ed0af5124a7a1b6d
Nehemias-Munoz <nehemiasmunozm@gmail.com>
2022-05-17 11:38:30 -0400

    Tipos de datos que existen en cup

--- basic_course_cpp/main.cpp
+++ basic_course_cpp/main.cpp
@@ -6,9 +6,22 @@
 //
 
 #include <iostream>
+using namespace::std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // tipos de datos primitivos
+    //int
+    cout << 10 << endl;
+    //float
+    cout << 3.1416f << endl;
+    //bool
+    cout << true << endl;
+    //char
+    cout << "H" << endl;
+    
+    //Datos compuestos
+    
+    //string
+    cout << "Hola \n" << endl; // \n = termino de linea
+
 }
