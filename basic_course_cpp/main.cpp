fe2e3c56b10d42f154398cf299ff21edc8a2ec2b
Nehemias-Munoz <nehemiasmunozm@gmail.com>
2022-05-17 11:30:34 -0400

    Initial Commit

---
+++ basic_course_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  basic_course_cpp
+//
+//  Created by Nehemias Muñoz on 17-05-22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
